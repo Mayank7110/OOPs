ecaab35eaaad97c7973375fd7e287d60eab26c2a
Mayank Wankhede <mayankwankhede258@gmail.com>
2025-07-30 23:40:20 +0530

    3 commit

--- oops.cpp
+++ oops.cpp
@@ -3,27 +3,45 @@
 using namespace std;
 
 class Teacher {
+
+private:
+    double salary; //data hiding
+
+public:
     //properties//attributes
  string name;
  string dept;
  string subject;
- double salary;
+ 
 
  //methods/member functions
 
  void changeDept(string newDept) {
     dept = newDept;
  }
+
+ //setter
+ void setSalary(double s){
+    salary =s;
+ }
+
+ //getter
+  double getSalary(){
+    return salary;
+ }
 };
 
+
+
 int main() {
-    Teacher t1;
+    Teacher t1; //Constructor Call
     t1.name = "Mayank";
     t1.subject = "C++";
     t1.dept = "CMPN";
-    t1.salary = 25000;
+    t1.setSalary(35000);
 
     cout << t1.name << endl;
+    cout << t1.getSalary() << endl;
     return 0;
 
 }
