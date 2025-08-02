77a2fabb3b8d3797b93871facdfc533c130e5963
Mayank Wankhede <mayankwankhede258@gmail.com>
2025-08-02 23:29:18 +0530

    5 commit

--- oops.cpp
+++ oops.cpp
@@ -2,68 +2,119 @@
 #include <string>
 using namespace std;
 
-class Teacher {
 
-private:
-    double salary; //data hiding
-
-public:
-
-    string name;
-    string dept;
-    string subject;
+class Student {
+   public:
     
-    //non-parameterized
-    // Teacher(){
-    //     dept = "CMPN";
-    // }
+   string name;
+   double* cgpaPtr;
 
-    //parameterized 
-    Teacher(string n, string d, string s, double sal){
-        name =n;
-        dept = d;
-        subject = s;
-        salary = sal;
-    }
-    //properties//attributes
+   Student(string name, double cgpa){
+      this->name = name;
+      cgpaPtr = new double;
+      *cgpaPtr = cgpa;
+   }
 
- 
+   Student(Student &obj){
+      this->name = obj.name;
+      cgpaPtr = new double;
+      *cgpaPtr = *obj.cgpaPtr;
+   }
 
- //methods/member functions
-
- void changeDept(string newDept) {
-    dept = newDept;
- }
-
- //setter
- void setSalary(double s){
-    salary =s;
- }
-
- //getter
-  double getSalary(){
-    return salary;
- }
-
- void getinfo(){
+    void getinfo(){
     cout << "name: " << name << endl;
-    cout << "subejct: " << subject << endl;
+    cout << "cgpa: " << *cgpaPtr << endl;
 
  }
 };
 
+int main(){
+   Student s1("mayank",8.4);
+   Student s2(s1);
+   s1.getinfo();
 
-
-int main() {
-    Teacher t1("Mayank" , "CMPN", "CPP", 44000); //Constructor Call
-    // t1.name = "Mayank";
-    // t1.subject = "C++";
-    // t1.setSalary(35000);
-
-    // cout << t1.dept << endl;
-    // cout << t1.getSalary() << endl;
-
-    t1.getinfo();
-    return 0;
-
+   *(s2.cgpaPtr) = 9.2;
+   s1.getinfo();
+   s2.name = "Tejas";
+   s2.getinfo();
+   return 0;
 }
+
+
+
+// class Teacher {
+
+// private:
+//     double salary; //data hiding
+
+// public:
+
+//     string name;
+//     string dept;
+//     string subject;
+    
+//     //non-parameterized
+//     // Teacher(){
+//     //     dept = "CMPN";
+//     // }
+
+//     //parameterized 
+//     Teacher(string name, string dept, string subject, double salary){
+//         this->name =name;
+//         this->dept = dept;
+//         this->subject = subject;
+//         this->salary = salary;
+//     }
+//    // copy constructor
+//    Teacher(Teacher &orgObj){//pass by reference
+//       cout << "I am custom copy constructor"<< endl;
+
+//          this->name = orgObj.name;
+//         this->dept = orgObj.dept;
+//         this->subject = orgObj.subject;
+//         this->salary = orgObj.salary;
+//    }
+
+ 
+
+//  //methods/member functions
+
+//  void changeDept(string newDept) {
+//     dept = newDept;
+//  }
+
+//  //setter
+//  void setSalary(double s){
+//     salary =s;
+//  }
+
+//  //getter
+//   double getSalary(){
+//     return salary;
+//  }
+
+//  void getinfo(){
+//     cout << "name: " << name << endl;
+//     cout << "subejct: " << subject << endl;
+
+//  }
+// };
+
+
+
+// int main() {
+//     Teacher t1("Mayank" , "CMPN", "CPP", 44000); //Constructor Call
+
+//     Teacher t2(t1); //default copy constructor
+
+//     // t1.name = "Mayank";
+//     // t1.subject = "C++";
+//     // t1.setSalary(35000);
+
+//     // cout << t1.dept << endl;
+//     // cout << t1.getSalary() << endl;
+
+//     t2.getinfo();
+//     return 0;
+
+// }
